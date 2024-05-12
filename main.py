32d057d63db2cfc8f066c39ae9b7e7502820dcf4
husna <husna1788@gmail.com>
2024-05-12 14:48:14 +0800

    adjustments on drawing for the bookcover

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+from flask import redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def bookcover():
-    return render_template("cover.html")
+    return render_template('cover.html')
+
+@app.route('/edit-book-cover')
+def edit_book_cover():
+    return render_template('edit_book_cover.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
