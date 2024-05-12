6fcfb37248f783b43d8f820c8d80537070de8cbc
husna <husna1788@gmail.com>
2024-05-12 10:48:56 +0800

    linking flask with html/css

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-hello word
-beep
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def bookcover():
+    return render_template("cover.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
