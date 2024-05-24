28628b95fdf714dadb620649625cdee765c19730
Jason <imjasonnnnn@gmail.com>
2024-05-24 22:38:14 +0800

    created a new file images

--- Text Size Customizable/main.py
+++ Text Size Customizable/main.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('text size customizable.html')
 
 @app.route('/customize', methods=['POST'])
 def customize():
     text_size = request.form['text_size']
-    return render_template('index.html', text_size=text_size)
+    return render_template('text size customizable.html', text_size=text_size)
 
 if __name__ == '__main__':
     app.run(debug=True)
