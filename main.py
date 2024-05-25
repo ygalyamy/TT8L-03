7aa13c4dd3936a157a540f6a705bb925d17a6322
husna <husna1788@gmail.com>
2024-05-25 19:01:57 +0800

    saving user's customizable cover

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template, request
-from flask import redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,jsonify
+import os
+import base64
 
 app = Flask(__name__)
 
+if not os.path.exists('static/images'):
+    os.makedirs('static/images')
+
 @app.route('/')
 def bookcover():
-    return render_template('cover.html')
+    image_path = 'images/bookcover.png'
+    image_exists = os.path.exists(os.path.join('static', image_path))
+    return render_template('cover.html', image_exists=image_exists, image_path=image_path)
 
-@app.route('/edit-book-cover', methods=['GET', 'POST'])
+@app.route('/edit-book-cover', methods=['GET'])
 def edit_book_cover():
-    if request.method == 'POST' and request.form.get('edit') == 'true':
-        return redirect(url_for('edit_book_cover'))
     return render_template('edit_book_cover.html')
+
+@app.route('/save_image', methods=['POST'])
+def save_image():
+    data = request.json
+    image_data = data['image'].split(",")[1]
+    image_data = base64.b64decode(image_data)
+    with open('static/images/bookcover.png', 'wb') as f:
+        f.write(image_data)
+    return jsonify(success=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
