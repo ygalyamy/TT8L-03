aaade60523e31bc67361a645dbf8dbc3ffc7605d
alya <yg.alyamysarah@gmail.com>
2024-06-19 14:55:29 +0800

    changed from py to js

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=20)])
-    submit = SubmitField('Login')
