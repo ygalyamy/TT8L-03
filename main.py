6a88598079b49689df25c467f681ca5a19ee3b3c
husna <husna1788@gmail.com>
2024-06-20 22:53:01 +0800

    updated book of the month

--- main.py
+++ main.py
@@ -21,25 +21,30 @@ def edit_book_cover():
 
 @app.route('/save_image', methods=['POST'])
 def save_image():
-    data = request.json
-    image_data = data['image'].split(",")[1]
-    image_data = base64.b64decode(image_data)
-    with open('static/images/bookcover.png', 'wb') as f:
-        f.write(image_data)
-    
-    with open('static/images/canvas_state.json', 'w') as f:
-        json.dump(data['canvasState'], f)
-
-    return jsonify(success=True)
+    try:
+        data = request.json
+        image_data = data['image'].split(",")[1]
+        image_data = base64.b64decode(image_data)
+        with open('static/images/bookcover.png', 'wb') as f:
+            f.write(image_data)
+        
+        with open('static/images/canvas_state.json', 'w') as f:
+            json.dump(data['canvasState'], f)
+        return jsonify(success=True)
+    except Exception as e:
+        return jsonify(success=False, error=str(e))
 
 @app.route('/load_canvas_state', methods=['GET'])
 def load_canvas_state():
-    canvas_state_path = 'static/images/canvas_state.json'
-    if os.path.exists(canvas_state_path):
-        with open(canvas_state_path, 'r') as f:
-            canvas_state = json.load(f)
-        return jsonify(success=True, canvasState=canvas_state)
-    return jsonify(success=False, message="No canvas state found.")
+    try:
+        canvas_state_path = 'static/images/canvas_state.json'
+        if os.path.exists(canvas_state_path):
+            with open(canvas_state_path, 'r') as f:
+                canvas_state = json.load(f)
+            return jsonify(success=True, canvasState=canvas_state)
+        return jsonify(success=False, message="No canvas state found.")
+    except Exception as e:
+        return jsonify(success=False, error=str(e))
 
 @app.route('/notification')
 def noti():
@@ -48,11 +53,6 @@ def noti():
 @app.route('/book-of-the-month')
 def bookofthemonth():
     return render_template("book_of_the_month.html")
-
-@app.route('/index')
-def index():
-    return render_template("index.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
